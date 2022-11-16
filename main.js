a03687b2222e4404629398d7126b0a6c6270853f
Muhammad Ahyaruddin <ahyaruddin07@gmail.com>
2022-11-17 00:21:53 +0700

    My L3 solution

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+
+            // My challenge solution
+            url: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flipkart.com%2Fmens-footwear%2Fcasual-shoes%2Fpr%3Fsid%3Dosp%2Ccil%2Ce1f&psig=AOvVaw3lYzAr0JimqGmNFEBmPJys&ust=1668705586723000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLjVgPCas_sCFQAAAAAdAAAAABAK'
         }
     }
 })
