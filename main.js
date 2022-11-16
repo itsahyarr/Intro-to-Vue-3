e70dc4429e99759cd9cac82a0aded72bf0080716
Muhammad Ahyaruddin <ahyaruddin07@gmail.com>
2022-11-16 23:56:58 +0700

    L2-challenge

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Learning',
+            description: 'I\'m very happy for learning.'
+        }
+    }
+})
