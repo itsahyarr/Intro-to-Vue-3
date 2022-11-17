ee3fdb4019414c0ac1cc4a45beb9e0e88bfd2585
Muhammad Ahyaruddin <ahyaruddin07@gmail.com>
2022-11-17 09:16:22 +0700

    adding solution

--- main.js
+++ main.js
@@ -8,7 +8,10 @@ const app = Vue.createApp({
             variants: [
                 {id: 2234, color: 'green'},
                 {id: 2235, color: 'blue'},
-            ]
+            ],
+
+            // My code challenge solution
+            sizes: ['S', 'M', 'L', 'XL']
         }
     }
 })
