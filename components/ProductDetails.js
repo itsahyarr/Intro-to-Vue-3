8618266c1216e357f7d3305a5be6d06928b04539
Muhammad Ahyaruddin <ahyaruddin07@gmail.com>
2022-11-21 11:22:11 +0700

    L9 solution

---
+++ components/ProductDetails.js
@@ -0,0 +1,11 @@
+app.component('product-details', {
+    props: {
+        details: {
+            type: String,
+            requred: true
+        }
+    },
+    template:
+    /*html*/
+    `<p>This is product details</p>`
+})
